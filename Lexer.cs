2c67c630d61c08de97dbc6a7c924fa8853276aee
collageso <dnjsqhwo@gmail.com>
2026-02-16 23:49:32 +0900

    feat: lexer class 구현 완료

--- Lexer.cs
+++ Lexer.cs
@@ -15,6 +15,26 @@ public class Lexer
     private int _startColumn = 1;
     private int _currentColumn = 1;
 
+    private static readonly Dictionary<string, TokenType> Keywords = new()
+    {
+        {"and", TokenType.AND},
+        {"class", TokenType.CLASS},
+        {"else", TokenType.ELSE},
+        {"false", TokenType.FALSE},
+        {"for", TokenType.FOR},
+        {"fun", TokenType.FUN},
+        {"if", TokenType.IF},
+        {"nil", TokenType.NIL},
+        {"or", TokenType.OR},
+        {"print", TokenType.PRINT},
+        {"return", TokenType.RETURN},
+        {"super", TokenType.SUPER},
+        {"this", TokenType.THIS},
+        {"true", TokenType.TRUE},
+        {"var", TokenType.VAR},
+        {"while", TokenType.WHILE}
+    };
+
 
     public Lexer(string source, DiagnosticManager diagnosticManager)
     {
@@ -55,6 +75,8 @@ public class Lexer
             case '=': AddToken(Match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL); break;
             case '<': AddToken(Match('<') ? TokenType.LESS_EQUAL : TokenType.LESS); break;
             case '>': AddToken(Match('>') ? TokenType.GREATER_EQUAL : TokenType.GREATER); break;
+            case '/': HandleSlash(); break;
+            case '"': HandleString(); break;
 
             case ' ':
             case '\r':
@@ -67,7 +89,19 @@ public class Lexer
                 break;
 
             default:
-                _diagnosticManager.ReportBadCharacter(_currentLine, _startColumn, ch);
+                if (IsDigit(ch))
+                {
+                    HandleNumber();
+                }
+                else if (IsAlpha(ch))
+                {
+                    HandleIdentifier();
+                }
+                else
+                {
+                    _diagnosticManager.ReportBadCharacter(_currentLine, _startColumn, ch);
+                }
+
                 break;
         }
     }
@@ -104,5 +138,86 @@ public class Lexer
 
         _currentIndex++;
         return true;
+    }
+
+    private char Peek() => IsAtEnd() ? '\0' : _source[_currentIndex];
+
+    private char PeekNext() => _currentIndex + 1 >= _source.Length ? '\0' : _source[_currentIndex + 1];
+
+    private bool IsDigit(char ch) => ch >= '0' && ch <= '9';
+
+    private bool IsAlpha(char ch) => (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || ch == '_';
+
+    private bool IsAlphaNumeric(char ch) => IsAlpha(ch) || IsDigit(ch);
+
+    private void HandleSlash()
+    {
+        if (Match('/'))
+        {
+            while (Peek() != '\n' && !IsAtEnd())
+            {
+                Advance();
+            }
+        }
+        else
+        {
+            AddToken(TokenType.SLASH);
+        }
+    }
+
+    private void HandleString()
+    {
+        while (Peek() != '"' && !IsAtEnd())
+        {
+            if (Peek() == '\n')
+            {
+                _currentLine++;
+            }
+            Advance();
+        }
+
+        if (IsAtEnd())
+        {
+            _diagnosticManager.ReportUnterminatedString(_currentLine, _startColumn);
+        }
+
+        Advance();
+
+        string value = _source.Substring(_startIndex + 1, _currentIndex - _startIndex - 2);
+        AddToken(TokenType.STRING, value);
+    }
+
+    private void HandleNumber()
+    {
+        while (IsDigit(Peek()))
+        {
+            Advance();
+        }
+
+        if (Peek() == '.' && IsDigit(PeekNext()))
+        {
+            Advance();
+
+            while (IsDigit(Peek()))
+            {
+                Advance();
+            }
+        }
+
+        double value = double.Parse(_source.Substring(_startIndex, _currentIndex - _startIndex), System.Globalization.CultureInfo.InvariantCulture);
+        AddToken(TokenType.NUMBER, value);
+    }
+
+    private void HandleIdentifier()
+    {
+        while (IsAlphaNumeric(Peek()))
+        {
+            Advance();
+        }
+
+        string text = _source.Substring(_startIndex, _currentIndex - _startIndex);
+        TokenType type = Keywords.GetValueOrDefault(text, TokenType.IDENTIFIER);
+
+        AddToken(type);
     }
 }
